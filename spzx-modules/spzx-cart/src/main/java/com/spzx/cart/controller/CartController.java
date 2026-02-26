761282877f1b74a6ebf38b3d874691db0947a1ff
Byakuya-Rinne <2353845636@qq.com>
2026-02-26 18:46:52 +0800

    添加商品sku到购物车

--- spzx-modules/spzx-cart/src/main/java/com/spzx/cart/controller/CartController.java
+++ spzx-modules/spzx-cart/src/main/java/com/spzx/cart/controller/CartController.java
@@ -2,8 +2,13 @@ package com.spzx.cart.controller;
 
 import com.spzx.cart.service.ICartService;
 import com.spzx.common.core.web.controller.BaseController;
+import com.spzx.common.core.web.domain.AjaxResult;
+import com.spzx.common.security.annotation.RequiresLogin;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +19,33 @@ public class CartController extends BaseController {
 
     @Autowired
     private ICartService cartService;
+
+    @Operation( summary = "添加")
+    @RequiresLogin
+    @GetMapping("/addToCart/{skuId}/{num}")
+    public AjaxResult addCart(@PathVariable Long skuId, @PathVariable Integer num){
+        cartService.addCart(skuId, num);
+        return success("添加成功");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    @Operation( summary = "删除")
+    @Operation( summary = "修改")
+    @Operation( summary = "查询")
+
 
 }
--- spzx-modules/spzx-cart/src/main/java/com/spzx/cart/service/impl/CartServiceImpl.java
+++ spzx-modules/spzx-cart/src/main/java/com/spzx/cart/service/impl/CartServiceImpl.java
@@ -1,16 +1,60 @@
 package com.spzx.cart.service.impl;
 
+import com.spzx.cart.api.RemoteCartService;
+import com.spzx.cart.api.domain.CartInfo;
 import com.spzx.cart.service.ICartService;
+import com.spzx.common.core.constant.SecurityConstants;
+import com.spzx.common.core.context.SecurityContextHolder;
+import com.spzx.common.core.domain.R;
+import com.spzx.common.core.exception.ServiceException;
+import com.spzx.product.api.RemoteProductService;
+import com.spzx.product.api.domain.ProductSku;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
+
 @Service
 @Slf4j
 public class CartServiceImpl implements ICartService {
+    //购物车存在redis里, hash存储, 以固定字符串+userID分组(key), skuId为HashKey, CartInfo为value
 
     @Autowired
     RedisTemplate redisTemplate;
 
+    @Autowired
+    RemoteProductService remoteProductService;
+
+    private String getHashKey(){
+        return "user:cart:" + SecurityContextHolder.getUserId();
+    }
+
+    @Override
+
+    public void addCart(Long skuId, Integer num) {
+        String key = getHashKey();
+        //查询redis, 现在购物车里有没有该sku
+        CartInfo cartInfo = (CartInfo) redisTemplate.opsForHash().get(key, skuId.toString() );
+        if ( null != cartInfo){ //如果购物车里已有
+            Integer skuNum = cartInfo.getSkuNum();
+            cartInfo.setSkuNum( (skuNum += num) > 99 ? 99 : (skuNum + num));
+        }else { //如果购物车里没有这个东西, 根据skuId查询CartInfo, 再放到redis里
+            R<ProductSku> productSkuR = remoteProductService.getProductSku(skuId, SecurityConstants.INNER);
+            if (R.FAIL == productSkuR.getCode()){
+                throw new ServiceException(productSkuR.getMsg());
+            }
+            ProductSku productSku = productSkuR.getData();
+            cartInfo = new CartInfo();
+            cartInfo.setUserId( SecurityContextHolder.getUserId() );
+            cartInfo.setSkuId(skuId);
+            cartInfo.setCartPrice(productSku.getSalePrice());
+            cartInfo.setSkuPrice(productSku.getMarketPrice());
+            cartInfo.setSkuNum(1);
+            cartInfo.setSkuName(productSku.getSkuName());
+            cartInfo.setThumbImg(productSku.getThumbImg());
+        }
+        redisTemplate.opsForHash().put( key, skuId, cartInfo);
+    }
 }
